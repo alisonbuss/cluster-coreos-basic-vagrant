faab6ed847fb1209f414296a66c6b428dce82c21
alisonbuss <alisonbuss.dev@gmail.com>
2018-02-27 05:07:01 -0300

    add init... 52f52sd2

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Project: cluster-coreos-ansible-vagrant
+
+## Esse projeto se utiliza de um Vagrant Box gerado pelo projeto: **[coreos-packer](https://github.com/alisonbuss/coreos-packer/)**.
+
+# ...
---
+++ provision/shell-script/config-storage-unit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#-----------------------|DOCUMENTATION|-----------------------#
+# @descr:
+# @fonts: 
+#-------------------------------------------------------------#
+
+# @descr: Main function of the script, it runs automatically on the script call.
+# @param: 
+#    $@ | array: (*)
+function StartConfiguration {
+
+    printf '%b\n' "Initializing the (Storage Unit) configuration on the system...";
+
+    printf '%b\n' "--WARNING: Configuration not implemented!!!...";
+
+}
+
+# @descr: Call of execution of the script's main function.
+StartConfiguration "$@";
+
+# @descr: Finishing the script!!! :P
+exit 0;
